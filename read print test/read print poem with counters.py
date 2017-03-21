a630d017a3672947fbad1b3263618543f9c6f3ab
Brandon Ellington <brandone94477@gmail.com>
2017-03-21 09:29:40 -0400

    Create read print poem with counters.py

---
+++ read print test/read print poem with counters.py
@@ -0,0 +1,32 @@
+name_of_mydocument = 'tuesdayafternoon.txt'                
+file_input = open(name_of_mydocument, 'r')           
+
+line = file_input.readline()                          
+print(line, end = "")
+line = file_input.readline()                         
+print(line)
+line = file_input.readline()                         
+print(line)
+
+line = file_input.readline()                          
+
+line_counter = 1                                    
+stanza_counter = 1
+
+while line != '':                                     
+
+    if line == '\n':                                    
+      stanza_counter += 1                                      
+      print ()                                              
+    else:                                                
+      print(line_counter, end = '') 
+      print(")   ", end = "")
+      print(line, end = '') 
+      line_counter += 1                                    
+    line = file_input.readline()                        
+    
+print ()                                               
+print ()                         
+print ("The number of stanzas is ",  stanza_counter)   
+
+file_input.close()
