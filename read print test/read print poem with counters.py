e0ee2e47da00e9e8ff0afb0cb77be618db2c2e25
Brandon Ellington <brandone94477@gmail.com>
2017-03-23 09:39:38 -0400

    Update read print poem with counters.py

--- read print test/read print poem with counters.py
+++ read print test/read print poem with counters.py
@@ -12,23 +12,27 @@ line = file_input.readline()
 
 line_counter = 1                                    
 stanza_counter = 1
+total_lines_in_file = 2
 
 while line != '':                                     
-    if line == '\n':                                    
+  total_lines_in_file += 1
+  if line == '\n':                                    
       stanza_counter += 1                                      
       print ()                                              
-    else:                                                
+  else:                                                
       print(line_counter, end = '')
       if line_counter >= 10:
         print(")  ", line, end = "")
       else:
         print(")   ", line, end = "")
       line_counter += 1  
-                                    
-    line = file_input.readline()                        
+ 
+  line = file_input.readline()                        
     
 print ()                                               
 print ()                         
 print ("The number of stanzas is ",  stanza_counter)   
-
-file_input.close()                        
+print ("The total number of lines in this file are", total_lines_in_file)
+print ("The song Tuesday Afternoon first appeared in the album Days Of Future Passed which was released in 1967.")
+print ("The members of the Moody Blues are Mike Pinder, Graeme Edge, Justin Hayward, Ray Thomas and John Lodge.")
+file_input.close()
